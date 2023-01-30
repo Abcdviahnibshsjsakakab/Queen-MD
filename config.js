a2717f4fa245649102ba53403907a260e6cccce3
VISHNU-Aaha-leech <123584095+VISHNU-Aaha-leech@users.noreply.github.com>
2023-01-30 21:52:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 global.session = process.env.SESSION_ID || 'PUT HERE',
-global.ownername = process.env.OWNER_NAME === undefined ? "DarkWinzo" : process.env.OWNER_NAME
-let fake = process.env.OWNER_NUMBER === undefined ? '94775200935' : process.env.OWNER_NUMBER
+global.ownername = process.env.OWNER_NAME === undefined ? "VisHnuU" : process.env.OWNER_NAME
+let fake = process.env.OWNER_NUMBER === undefined ? '8943699836' : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
 global.ownernumber = [`${fake}`]
 global.premium = [`${fake}`]
 global.anticall = process.env.ANTICALL === undefined ? 'false' : process.env.ANTICALL
 global.packname = process.env.PACK_NAME === undefined ? 'Queen-MD' : process.env.PACK_NAME
-global.author = process.env.STICKER_AUTHOR_NAME === undefined ? 'DarkWinzo' : process.env.STICKER_AUTHOR_NAME
+global.author = process.env.STICKER_AUTHOR_NAME === undefined ? 'VIshnu' : process.env.STICKER_AUTHOR_NAME
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 global.Botname = 'Q'+'ᴜ'+'ᴇ'+'ᴇ'+'ɴ'+'-'+'ᴍ'+'ᴅ'
